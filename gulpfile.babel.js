be388d079b66fc53030ca2e887e2a1195ad1dd51
Joshua Rutherford <josh_rutherford@apple.com>
2016-03-15 17:02:20 -0700

    initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,36 @@
+import gulp from 'gulp';
+import scss from 'gulp-sass';
+import cssMin from 'gulp-minify-css';
+import rename from 'gulp-rename';
+import prefix from 'gulp-autoprefixer';
+import ugly from 'gulp-uglify';
+import del from 'del';
+
+const OUTPUT_DIR = './dist';
+const SCRIPT_DIR = './src/scripts/flipbook.js';
+const STYLE_DIR = './src/css/scss/main.scss';
+
+gulp.task( 'default', ['clean', 'scripts', 'css'] );
+
+gulp.task( 'clean', () => {
+    del( OUTPUT_DIR );
+});
+
+gulp.task( 'scripts', () => {
+    return gulp.src( SCRIPT_DIR )
+        .pipe( babel( {
+            presets: ['es2015']
+        }))
+        .pipe( ugly() )
+        .pipe( rename( 'main.js' ) )
+        .pipe( gulp.dest( OUTPUT_DIR ) );
+});
+
+gulp.task( 'css', () => {
+    return gulp.src( STYLE_DIR )
+        .pipe( scss() )
+        .pipe( prefix() )
+        .pipe( cssMin() )
+        .pipe( rename( 'main.css' ) )
+        .pipe( gulp.dest( OUTPUT_DIR ) );
+});
